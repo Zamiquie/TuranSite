b05c32a9af9d42628f2071528897719a2e6cfa1e
DAVID-AHIALEY\educt <eductora6@gmail.com>
2018-03-14 11:40:44 +0100

    Mise en place du projet + listage d'images

--- Galerie.php
+++ Galerie.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: educt
- * Date: 14/03/2018
- * Time: 11:11
- */
+# Nom du dossier images à renseigner
+$dir = 'assets/galerie';
+# Récupération et affichage de la liste des images sous forme de liste
+$glob = plxGlob::getInstance($dir);
+
+if ($files = $glob->query('/[a-z0-9-_]+.tb.(jpg|gif|png)$/i')) {
+    echo '<ul class="galerie">';
+    foreach($files as $filename) {
+        $file = $dir.'/'.$filename;
+        echo '<li><a href="'.str_replace('.tb', '', $file).'"><img src="'.$file.'" alt="" /></a></li>';
+    }
+    echo '</ul>';
+}
+else echo "Aucune image";
+?>
