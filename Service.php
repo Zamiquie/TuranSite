2a3f4ad659085bcfad0a3df467be1721ac553491
zamiquie <aymericbaquet@gmail.com>
2018-03-14 16:44:39 +0100

    Mise en Place CArroussel
    Mise en Place Carroussel

--- Service.php
+++ Service.php
@@ -4,13 +4,20 @@
 <head>
     <link  rel="stylesheet" type="text/css" href="assets\css.css" />
     <link rel="icon" type="image/gif" href="assets\ico.gif" />
+
     <title> SERVICE </title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css.css">
 </head>
 
 <body>
 <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="index.php">Accueil</a>
     <a class="navbar-brand" href="Service.php">Service</a>
+    <a class="navbar-brand" href="Galerie.php">Galerie</a>
     <a class="navbar-brand" href="Contact.php">Contact</a>
 </nav>
 
--- index.php
+++ index.php
@@ -4,18 +4,45 @@
     <head>
         <link  rel="stylesheet" type="text/css" href="assets\css.css" />
         <link rel="icon" type="image/gif" href="assets\ico.gif" />
+
         <title> ACCUEIL </title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+        <link rel="stylesheet" href="assets/css.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
     <body>
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="index.php">Accueil</a>
         <a class="navbar-brand" href="Service.php">Service</a>
+        <a class="navbar-brand" href="Galerie.php">Galerie</a>
         <a class="navbar-brand" href="Contact.php">Contact</a>
     </nav>
 
-    <h1> Presentation</h1>
-        <p> Notre savoir faire</p>
+    <div id="slide" class="carousel slide" data-ride="carousel" data-interval="5000" >
+
+        <div class="carousel-inner" >
+            <div class="carousel-item active">
+                <img class="d-block w-100"  src="assets\galerie\park.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets\galerie\coast.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets\galerie\bridge.jpg" alt="Third slide">
+            </div>
+        </div>
+    </div>
+
+    <nav class="baguetteBox-button">
+        <a class="navbae navbar-light bg-light" href="Mentions.php">Mentions Legales</a>
+    </nav>
     </body>
 
 </html>
